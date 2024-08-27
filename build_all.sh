d45d23ee20c5c8ea5bba78927c63768980c290d0
Christopher Tessum <ctessum@gmail.com>
2024-08-27 08:50:16 -0500

    Update Docker images for Julia 1.10

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-#export platform="linux/arm64/v8,linux/amd64"
-#export platform="linux/arm64/v8"
-export platform="linux/amd64" # Cross-compilation not working.
+export platform="linux/arm64/v8,linux/amd64"
 
-tag="0.5.0"
+tag="0.6.0"
 
-
-#for image in pl-julia-datascience-pluto-workspace
 for image in pl-julia-datascience-base pl-julia-datascience-grader pl-julia-datascience-pluto-workspace
 do
     echo "------------------- Building ${image} -------------------"
@@ -15,13 +11,3 @@ do
     docker buildx build --platform $platform --push --tag ctessum/${image}:latest --tag ctessum/${image}:${tag} .
     cd ..
 done
-
-# 0.2.0 time to working HW1: 12:41 (laggy even after this)
-# 0.3.0 time to working HW1: 6:41
-# 0.4.0 time to working HW1: 3:45
-
-# gcloud 
-# 0.2.0: 10s startup, 3.4GB memory
-# 0.3.0: 18s startup, 3.4GB memory
-# 0.3.1: 13s startup, 3.4GB memory
-# 0.4.0: 16s startup, ~600MB memory?
